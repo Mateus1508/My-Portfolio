1178eb960703281bc425a5ba343778aebf599544
Mateus <81775179+Mateus1508@users.noreply.github.com>
2022-07-15 23:39:15 -0300

    Update Contact.jsx

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React, {useRef, useEffect, useState} from 'react';
 import emailjs from '@emailjs/browser';
-import { ButtonLarge } from '../Buttons/buttonStyle';
+import { ButtonLarge } from './Components/Buttons/buttonStyle';
 
 import { ContactContainer, Footer } from './contactStyle';
 
