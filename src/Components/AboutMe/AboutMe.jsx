8ddceef6c1e4c15c9d416d8712e8336e50386a0e
Mateus Belmonte <mateus.santos@suporterenova.onmicrosoft.com>
2024-03-19 15:29:09 -0300

    fix: text on about me

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -9,7 +9,7 @@ const AboutMe = () => {
 				<img src="https://github.com/Mateus1508.png" alt="Mateus, Foto de perfil" />  
 				<section>
 					<h2>Quem sou</h2>
-					<p>Sou um Desenvolvedor Fullstack com experiência significativa em tecnologias como React, Typescript, Node e C#, e tenho uma afinidade especial com frameworks de estilo, como o Tailwind. Com mais de 2 anos de atuação no desenvolvimento de software, tenho adquirido experiência sólida em trabalhar com bancos de dados tanto SQL quanto NoSQL, e minhas habilidades são comprovadas por meus certificados e projetos.</p>
+					<p>Sou um Desenvolvedor Frontend com experiência significativa em tecnologias como React, Typescript, Node e C#, e tenho uma afinidade especial com frameworks de estilo, como o Tailwind. Com mais de 2 anos de atuação no desenvolvimento de software, tenho adquirido experiência sólida em trabalhar com bancos de dados tanto SQL quanto NoSQL, e minhas habilidades são comprovadas por meus certificados e projetos.</p>
 					<p>Meu compromisso é entregar soluções de alta qualidade de forma eficiente, sempre mantendo um padrão de excelência. Além disso, sou um colaborador ativo em equipes, sempre disposto a aprender e contribuir. Compartilhar conhecimentos e experiências com meus colegas é uma parte fundamental do meu trabalho.</p>
 				</section>
 			</div>
